326332304fa1600a792dd0be36d01fc1afc1c137
Steven Xu <steven.xu13@gmail.com>
2023-02-05 19:26:28 -0500

    Initial setup

---
+++ README.md
@@ -0,0 +1,45 @@
+# FRC Helper
+
+A Chrome extension for daily work at the Family Reception Centre.
+
+## Prerequisites
+
+* node
+* npm
+
+## Setup
+
+```
+npm install
+```
+
+## Build
+
+```
+npm run build
+```
+
+## Build in watch mode
+
+### terminal
+
+```
+npm run watch
+```
+
+### Visual Studio Code
+
+Run watch mode.
+
+type `Ctrl + Shift + B`
+
+## Load extension to chrome
+
+Load `dist` directory
+
+## Test
+`npx jest` or `npm run test`
+
+## Credit
+
+Project setup inspiration: https://github.com/chibat/chrome-extension-typescript-starter
---
+++ webpack/webpack.common.js
@@ -0,0 +1,38 @@
+const path = require("path");
+const CopyPlugin = require("copy-webpack-plugin");
+
+const srcDir = path.join(__dirname, "..", "src");
+
+module.exports = {
+  mode: "production",
+  entry: {
+    popup: path.join(srcDir, "popup.ts"),
+    content_script: path.join(srcDir, "content.ts"),
+  },
+  output: {
+    path: path.join(__dirname, "../dist/js"),
+    filename: "[name].js",
+  },
+  resolve: {
+    extensions: [".ts", ".js"],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        loader: "ts-loader",
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  plugins: [
+    new CopyPlugin({
+      patterns: [{
+        from: ".",
+        to: "../",
+        context: "public",
+        noErrorOnMissing: true
+      }]
+    }),
+  ],
+};
