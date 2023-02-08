30f80739c41537b0f9dd3fa5b4f5ebc9859114ca
Steven Xu <steven.xu13@gmail.com>
2023-02-07 22:53:54 -0500

    Store family info on button click

--- src/school_interviews/details_page.ts
+++ src/school_interviews/details_page.ts
@@ -8,13 +8,67 @@ function insertButton(table: HTMLTableElement) {
   fetch(chrome.runtime.getURL('/html/details_page.html')).then(r => r.text()).then(html => {
     table.insertAdjacentHTML('beforebegin', html);
 
-    const family = new RawData(table).parse();
     const container = document.getElementsByClassName("offcanvas-body")[0];
-    container.innerHTML = DetailsPageNavHelpers.generate(family);
+    setupPage(container, table);
   });
 }
 
-const table: HTMLTableElement | null = document.querySelector("#container > div > section > table");
-if (table) {
-  insertButton(table);
+function setupPage(container: Element, table: HTMLTableElement) {
+  const familyId = getFamilyIdFromURL();
+  chrome.storage.local.get([familyId], (result) => {
+    let family: Family | null = result[familyId];
+    if (family) {
+      console.log(`details_page.ts: family with id ${familyId} found: ${JSON.stringify(family)}`);
+      // Class instances are stored as serialized objects, so we need to convert them back to class instances
+      family = Object.assign(new Family(), family);
+    } else {
+      console.log(`details_page.ts: family with id ${familyId} not found.`);
+      family = new RawData(table).parse();
+    }
+
+    container.innerHTML = DetailsPageNavHelpers.generate(family);
+    setupButton(familyId, result[familyId] == null, family);
+  });
+}
+
+function setupButton(familyId: string, isNewFamily: boolean, family: Family) {
+  const button = document.getElementById("addNewFamily");
+  if (!button) {
+    throw new Error("details_page.ts: Could not find #addNewFamily.");
+  }
+
+  function buttonOnClick() {
+    family.uniqueId = familyId;
+    chrome.storage.local.set({ [familyId]: family }, () => {
+      console.log(`details_page.ts: Successfully saved family ${familyId} to local storage.`);
+
+      // Update the button to say "View Family" and remove the click listener
+      setupButton(familyId, false, family);
+      button?.removeEventListener("click", buttonOnClick);
+    });
+  }
+
+  if (isNewFamily) {
+    button.innerText = "Add New Family";
+    button.addEventListener("click", buttonOnClick);
+  } else {
+    button.innerText = "View Family";
+  }
+}
+
+function getFamilyIdFromURL() {
+  const pathnames = location.pathname.split("/");
+  if (pathnames.length == 5) {
+    return pathnames[3];
+  }
+
+  throw new Error("details_page.ts: Could not get family id.");
+}
+
+// make sure we're on the details page
+if (/\/parents\/\d+\/details$/.test(location.pathname)) {
+  const table: HTMLTableElement | null = document.querySelector("#container > div > section > table");
+  if (table) {
+    insertButton(table);
+  }
 }
--- src/school_interviews/models/family.ts
+++ src/school_interviews/models/family.ts
@@ -4,6 +4,9 @@ export class Family {
   parents: Person[] = [];
   students: Student[] = [];
 
+  // this is the number in the URL, e.g. /parents/4936708732223488/details
+  uniqueId: string = "";
+
   get people(): Person[] {
     return this.parents.concat(this.students);
   }
