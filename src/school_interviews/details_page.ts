8acb22ce8e538bbc971566718bf63ac395151af4
Steven Xu <steven.xu13@gmail.com>
2023-02-08 22:06:10 -0500

    Functional update button for each person's form

--- src/school_interviews/details_page.ts
+++ src/school_interviews/details_page.ts
@@ -1,29 +1,29 @@
 import { DetailsPageNavHelpers } from "./helpers/details_page_nav_helper";
 import { FamilyRepository } from "./helpers/family_repository";
 import { Family } from "./models/family";
+import { Student } from "./models/person";
 import { RawData } from "./models/raw_data";
 
-function insertButton(table: HTMLTableElement) {
-  // if family exists, view family
-  // otherwise, add new family
+function patchDetailsPage(table: HTMLTableElement) {
   fetch(chrome.runtime.getURL('/html/details_page.html')).then(r => r.text()).then(html => {
     table.insertAdjacentHTML('beforebegin', html);
 
     const container = document.getElementsByClassName("offcanvas-body")[0];
-    setupPage(container, table);
+    setupOffCanvasPage(container, table);
   });
 }
 
-function setupPage(container: Element, table: HTMLTableElement) {
+function setupOffCanvasPage(container: Element, table: HTMLTableElement) {
   const familyId = getFamilyIdFromURL();
   FamilyRepository.getFamilyWithUniqueId(familyId).then(family => {
     const aFamily = family || new RawData(table).parse();
     container.innerHTML = DetailsPageNavHelpers.generate(aFamily);
-    setupButton(familyId, family == null, aFamily);
+    setupPrimaryButton(familyId, family == null, aFamily);
+    setupForms(aFamily);
   });
 }
 
-function setupButton(familyId: string, isNewFamily: boolean, family: Family) {
+function setupPrimaryButton(familyId: string, isNewFamily: boolean, family: Family) {
   const button = document.getElementById("addNewFamily");
   if (!button) {
     throw new Error("details_page.ts: Could not find #addNewFamily.");
@@ -33,7 +33,7 @@ function setupButton(familyId: string, isNewFamily: boolean, family: Family) {
     family.uniqueId = familyId;
     FamilyRepository.saveFamily(family).then(() => {
       // Update the button to say "View Family" and remove the click listener
-      setupButton(familyId, false, family);
+      setupPrimaryButton(familyId, false, family);
       button?.removeEventListener("click", buttonOnClick);
     });
   }
@@ -44,6 +44,29 @@ function setupButton(familyId: string, isNewFamily: boolean, family: Family) {
   } else {
     button.innerText = "View Family";
   }
+}
+
+function setupForms(family: Family) {
+  const forms = document.querySelectorAll(".offcanvas-body > .tab-content > .tab-pane > form");
+  (Array.from(forms) as HTMLFormElement[]).forEach((form, index) => {
+    const person = family.people[index];
+    form.addEventListener("submit", (event) => {
+      event.preventDefault();
+      const formData = new FormData(form);
+      person.name = formData.get("name") as string;
+      person.email = formData.get("email") as string;
+      person.phone = formData.get("phone") as string;
+      person.address = formData.get("address") as string;
+      person.extraNotes = formData.get("extraNotes") as string;
+      if (person instanceof Student) {
+        person.dateOfBirth = formData.get("dateOfBirth") as string;
+        person.countryOfBirth = formData.get("countryOfBirth") as string;
+        person.studentNotes = formData.get("studentNotes") as string;
+      }
+      family.people[index] = person;
+      FamilyRepository.saveFamily(family);
+    });
+  });
 }
 
 function getFamilyIdFromURL() {
@@ -59,6 +82,6 @@ function getFamilyIdFromURL() {
 if (/\/parents\/\d+\/details$/.test(location.pathname)) {
   const table: HTMLTableElement | null = document.querySelector("#container > div > section > table");
   if (table) {
-    insertButton(table);
+    patchDetailsPage(table);
   }
 }
--- src/school_interviews/helpers/details_page_nav_helper.ts
+++ src/school_interviews/helpers/details_page_nav_helper.ts
@@ -42,33 +42,33 @@ export class DetailsPageNavHelpers {
         <div class="tab-pane fade ${active}" id="pills-person-${index}" role="tabpanel" aria-labelledby="pills-person-${index}-tab" tabindex="0">
           <form>
             <div class="form-floating">
-              <input type="text" class="form-control mb-2" id="nameInput" placeholder="name" value="${person.name}" style="background-color: var(--bs-form-control-bg); border:var(--bs-border-width) solid var(--bs-border-color);">
+              <input type="text" class="form-control mb-2" name="name" placeholder="name" value="${person.name}" style="background-color: var(--bs-form-control-bg); border:var(--bs-border-width) solid var(--bs-border-color);">
               <label for="nameInput">Name</label>
             </div>
 
             <div class="form-floating">
-              <input type="email" class="form-control mb-2" id="emailInput" placeholder="email" value="${person.email}" style="background-color: var(--bs-form-control-bg); border:var(--bs-border-width) solid var(--bs-border-color);">
+              <input type="email" class="form-control mb-2" name="email" placeholder="email" value="${person.email}" style="background-color: var(--bs-form-control-bg); border:var(--bs-border-width) solid var(--bs-border-color);">
               <label for="emailInput">Email</label>
             </div>
 
             <div class="form-floating">
-              <input type="tel" class="form-control mb-2" id="phoneInput" placeholder="phone" value="${person.phone}" style="background-color: var(--bs-form-control-bg); border:var(--bs-border-width) solid var(--bs-border-color);">
+              <input type="tel" class="form-control mb-2" name="phone" placeholder="phone" value="${person.phone}" style="background-color: var(--bs-form-control-bg); border:var(--bs-border-width) solid var(--bs-border-color);">
               <label for="phoneInput">Phone</label>
             </div>
 
             <div class="form-floating">
-              <input type="text" class="form-control mb-2" id="addressInput" placeholder="address" value="${person.address}" style="background-color: var(--bs-form-control-bg); border:var(--bs-border-width) solid var(--bs-border-color);">
+              <input type="text" class="form-control mb-2" name="address" placeholder="address" value="${person.address}" style="background-color: var(--bs-form-control-bg); border:var(--bs-border-width) solid var(--bs-border-color);">
               <label for="addressInput">Address</label>
             </div>
 
             <div class="form-floating">
-              <textarea class="form-control mb-2" id="extraNotesInput" placeholder="extra notes" style="max-width: 25em; height: auto;">${person.extraNotes}</textarea>
+              <textarea class="form-control mb-2" name="extraNotes" placeholder="extra notes" style="max-width: 25em; height: auto;">${person.extraNotes}</textarea>
               <label for="extraNotesInput">Extra Notes</label>
             </div>
 
             ${person instanceof Student ? this.generateExtraStudentFields(person) : ""}
 
-            <button type="submit" class="btn btn-primary">Update</button>
+            <button type="submit" class="btn btn-primary">Save</button>
           </form>
         </div>
       `;
@@ -80,17 +80,17 @@ export class DetailsPageNavHelpers {
   static generateExtraStudentFields(student: Student) {
     return `
       <div class="form-floating">
-        <input type="text" class="form-control mb-2" id="dateOfBirthInput" placeholder="date of birth" value="${student.dateOfBirth}" style="background-color: var(--bs-form-control-bg); border:var(--bs-border-width) solid var(--bs-border-color);">
+        <input type="text" class="form-control mb-2" name="dateOfBirth" placeholder="date of birth" value="${student.dateOfBirth}" style="background-color: var(--bs-form-control-bg); border:var(--bs-border-width) solid var(--bs-border-color);">
         <label for="dateOfBirthInput">Date of Birth</label>
       </div>
 
       <div class="form-floating">
-        <input type="text" class="form-control mb-2" id="countryOfBirthInput" placeholder="country of birth" value="${student.countryOfBirth}" style="background-color: var(--bs-form-control-bg); border:var(--bs-border-width) solid var(--bs-border-color);">
+        <input type="text" class="form-control mb-2" name="countryOfBirth" placeholder="country of birth" value="${student.countryOfBirth}" style="background-color: var(--bs-form-control-bg); border:var(--bs-border-width) solid var(--bs-border-color);">
         <label for="countryOfBirthInput">Country of Birth</label>
       </div>
 
       <div class="form-floating">
-        <textarea class="form-control" id="studentNotesInput" placeholder="student notes" style="max-width: 25em; height: auto;">${student.studentNotes}</textarea>
+        <textarea class="form-control" name="studentNotes" placeholder="student notes" style="max-width: 25em; height: auto;">${student.studentNotes}</textarea>
         <label for="studentNotesInput">Student Notes</label>
       </div>
     `;
