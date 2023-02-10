ede332616b7b498862f358c4ed8cdac8d530f75c
Steven Xu <steven.xu13@gmail.com>
2023-02-10 15:23:26 -0500

    Update file size limit warning

--- webpack/webpack.prod.js
+++ webpack/webpack.prod.js
@@ -2,5 +2,9 @@ const { merge } = require('webpack-merge');
 const common = require('./webpack.common.js');
 
 module.exports = merge(common, {
-  mode: 'production'
+  mode: 'production',
+  performance: {
+    maxEntrypointSize: 512000,
+    maxAssetSize: 512000
+  }
 });
