088bca4a45a6b75a2ce161a25b80bdb9d1c40cd5
Steven Xu <steven.xu13@gmail.com>
2023-02-13 19:13:24 -0500

    Use title case for names

--- src/school_interviews/helpers/name_helper.ts
+++ src/school_interviews/helpers/name_helper.ts
@@ -1,10 +1,19 @@
 export class NameHelper {
-  static nameToParts(name: string): [string, string, string] {
+  static nameToParts(name: string): string[] {
     const parts = name.split(" ");
     return [
       parts.length > 0 ? parts[0] : "",
       parts.length > 2 ? parts.slice(1, parts.length - 1).join(" ") : "",
       parts.length > 1 ? parts[parts.length - 1] : "",
-    ];
+    ].map(s => NameHelper.toTitleCase(s));
+  }
+
+  static toTitleCase(str: string) {
+    return str.replace(
+      /\w\S*/g,
+      function(txt) {
+        return txt.charAt(0).toUpperCase() + txt.substring(1).toLowerCase();
+      }
+    );
   }
 }
